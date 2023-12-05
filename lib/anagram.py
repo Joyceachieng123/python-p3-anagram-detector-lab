469b918a36ae1c834530bbbc63bf5d745c97a545
Joyce Achieng <joyce.achieng@student.moringaschool.com>
2023-12-05 08:10:59 +0300

    Complete Assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, words):
+        word_sorted = sorted(self.word)
+        return [w for w in words if self.is_anagram(w, word_sorted)]
+
+    def is_anagram(self, word, sorted_word):
+        word = word.lower()
+        if word == self.word:
+            return False
+        return sorted(word) == sorted_word
